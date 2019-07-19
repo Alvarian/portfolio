da7d8b12006853783aa33b7e394ce2d5886d108c
Ivan <vitarka88@gmail.com>
2019-07-19 05:23:52 -0400

    add mysql functionality to register and post. both incomplete

---
+++ db/schemas/project_schema.sql
@@ -0,0 +1,23 @@
+-- CREATE py_port
+
+CREATE TABLE master(
+	id int AUTO_INCREMENT,
+	user VARCHAR(35),
+	password VARCHAR(100),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE projects(
+	id int AUTO_INCREMENT, 
+	app_type VARCHAR(255), 
+	deployed_url VARCHAR(255), 
+	description VARCHAR(255), 
+	game_file VARCHAR(255), 
+	git_url VARCHAR(255), 
+	icon_file VARCHAR(255), 
+	style_file VARCHAR(255), 
+	title VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+
+-- mysql -u root -p py_port < C:\Users\Administrator\Desktop\Coding\webDev\deployed_apps\portfolio\flask_v\db\schemas\project_schema.sql
