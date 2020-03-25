edf8a2216a74c896e06599b4b355cf6954e64da2
alvarian <vitarka88@gmail.com>
2020-03-25 18:29:23 -0400

    env update, only fetching from db, rest of crud disabled

--- config/envSwitch.py
+++ config/envSwitch.py
@@ -3,26 +3,10 @@ import os
 
 class keys:
 	def __init__(self):
-		if 'ACCESS_KEY_ID' in os.environ:
-			# HEROKU
-			self.KEY_ID = os.environ['ACCESS_KEY_ID']
-			self.SECRET_KEY = os.environ['ACCESS_SECRET_KEY']
-			self.REGION = os.environ['REGION_NAME']
-			self.BUCKET = os.environ['BUCKET_NAME']
+		from dotenv import load_dotenv
+		load_dotenv()
 
-			self.MASTER = os.environ['EXPECTED_MASTER']
-
-			self.MAIL_SERVER = os.environ['MAIL_SERVER']
-			self.MAIL_PORT = os.environ['MAIL_PORT']
-			self.MAIL_USERNAME = os.environ['MAIL_USERNAME']
-			self.MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
-			self.MAIL_USE_TLS = os.environ['MAIL_USE_TLS']
-			self.MAIL_USE_SSL = os.environ['MAIL_USE_SSL']
-
-			self.DB2_KEY = os.environ['DB2_KEY']
-		else:
-			from dotenv import load_dotenv
-			load_dotenv()
+		if os.getenv("IS_LOCAL"):
 
 			self.KEY_ID = os.getenv("ACCESS_KEY_ID")
 			self.SECRET_KEY = os.getenv("ACCESS_SECRET_KEY")
@@ -38,4 +22,22 @@ class keys:
 			self.MAIL_USE_TLS = os.getenv("MAIL_USE_TLS")
 			self.MAIL_USE_SSL = os.getenv("MAIL_USE_SSL")
 
-			self.DB2_KEY = os.getenv("DB2_KEY")
+			self.DATABASE_URL = os.getenv("DATABASE_URL")
+			self.IS_LOCAL = os.getenv("IS_LOCAL")
+		else:
+			# HEROKU
+			self.KEY_ID = os.environ['ACCESS_KEY_ID']
+			self.SECRET_KEY = os.environ['ACCESS_SECRET_KEY']
+			self.REGION = os.environ['REGION_NAME']
+			self.BUCKET = os.environ['BUCKET_NAME']
+
+			self.MASTER = os.environ['EXPECTED_MASTER']
+
+			self.MAIL_SERVER = os.environ['MAIL_SERVER']
+			self.MAIL_PORT = os.environ['MAIL_PORT']
+			self.MAIL_USERNAME = os.environ['MAIL_USERNAME']
+			self.MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
+			self.MAIL_USE_TLS = os.environ['MAIL_USE_TLS']
+			self.MAIL_USE_SSL = os.environ['MAIL_USE_SSL']
+
+			self.DATABASE_URL = os.environ['DATABASE_URL']
