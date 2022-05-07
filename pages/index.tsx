61f1a00a4f9ed3284bf6acc2d6b78cf7e9bf6c3f
Alvarian <alvarivan88@gmail.com>
2022-05-07 13:44:25 -0400

    style navbar menu fonts and padding, assigned width hook to index to distribute to all sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Section from 'components/section'
 import Border from 'components/border'
 import Footer from 'UI/footer'
 import Navbar from 'UI/navbar'
+import { useResize } from 'hooks/'
 
 import { Content } from 'lib/sections/sections.types'
 import { sectionData } from 'lib/sections/sections.data'
@@ -13,7 +14,7 @@ import { sectionData } from 'lib/sections/sections.data'
 
 const Home: NextPage = () => {
   const beginning = useRef<any>(null)
-
+  const [width] = useResize();
   const [visible, setVisible] = useState(false);
 
   const handleScroll = () => {
@@ -36,6 +37,7 @@ const Home: NextPage = () => {
       if (parseInt(i) % 2 !== 0) {
         sectionList.push(<Border
           key={`${i}_border`}
+          width={width}
           thickness="h-40"
           color="bg-black"
         />)
@@ -44,7 +46,7 @@ const Home: NextPage = () => {
       const section: Content = sectionData[i]
       sectionList.push(<Section
         key={i}
-        position={i}
+        width={width}
         setRef={parseInt(i) === 1 ? beginning : null}
         content={section.content}
         bgImageName={section.bgImageName}
@@ -55,6 +57,7 @@ const Home: NextPage = () => {
 
     sectionList.push(<Border
       key="last"
+      width={width}
       thickness="h-40"
       color="bg-black"
     />)
@@ -80,12 +83,12 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.tailwind.content}>
-        <Navbar visible={visible} />
+        <Navbar visible={visible} width={width} />
 
         {handleSectionRendering()}
       </main>
 
-      <Footer />
+      <Footer width={width} />
     </div>
   )
 }
