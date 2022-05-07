6957283cf6766643c6462c6d23b26b8a255ffe69
Alvarian <alvarivan88@gmail.com>
2022-05-07 17:22:48 -0400

    have switch for mobile width and default width for header and footer using width hook

--- components/underline.tsx
+++ components/underline.tsx
@@ -7,7 +7,7 @@ const Underline: React.FC<any> = ({
     width
 }) => {
     return (
-        <svg version="1.1" id="line_2" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width={width} height="10px" className="rounded-sm mt-4">
+        <svg version="1.1" id="line_2" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width={width || "120px"} height="10px" className="rounded-sm mt-4">
             <motion.path fill="#01a09e" strokeWidth="8" stroke="#FFFFFF" d="M0 0 l1120 0"
                 initial="hidden"
                 whileInView="visible" 
