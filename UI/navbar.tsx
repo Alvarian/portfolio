940b69a37985b91904975d9b69508a10c0db4dc5
Alvarian <alvarivan88@gmail.com>
2022-05-07 13:44:25 -0400

    style navbar menu fonts and padding, assigned width hook to index to distribute to all sections

--- UI/navbar.tsx
+++ UI/navbar.tsx
@@ -2,20 +2,20 @@ import Image from 'next/image'
 
 import { sectionData, defaultVariants } from 'lib/sections/sections.data'
 
-import { useResize } from 'hooks/'
 import Link from 'next/link'
 import { motion } from 'framer-motion'
 
 
 const Navbar: React.FC<any> = ({
-  visible
+  visible,
+  width
 }) => {
-  const [width] = useResize();
-
-  const handleRenderLinks = () => {
+  const handleRenderLinks = (textStyles: string) => {
     return sectionData.map((section, index) => (
-      <li className="text-white" key={index}>
-        <Link href={"#"+section.alt}>{section.alt.charAt(0).toUpperCase() + section.alt.slice(1)}</Link>
+      <li key={index}>
+        <Link href={"#"+section.alt}>
+          <span className={textStyles}>{section.alt.charAt(0).toUpperCase() + section.alt.slice(1)}</span>
+        </Link>
       </li>
     ))
   }
@@ -46,17 +46,17 @@ const Navbar: React.FC<any> = ({
     >
       <div className="navbar-start pl-10">
         {width > 800 ? 
-          <ul className="menu menu-horizontal text-xl shadow bg-black">
-            {handleRenderLinks()}
+          <ul className="menu menu-horizontal shadow bg-black">
+            {handleRenderLinks("text-xl")}
           </ul>
           :
-          <div className="dropdown pl-10">
+          <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost btn-circle hover:invert text-white">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-14 w-14" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" d="M4 6h16M4 12h16M4 18h7" /></svg>
             </label>
 
-            <ul className="menu menu-compact dropdown-content rounded-box mt-7 text-2xl shadow bg-black">
-              {handleRenderLinks()}
+            <ul className="menu menu-compact dropdown-content rounded-box mt-7 p-3 shadow bg-black">
+              {handleRenderLinks("text-3xl font-bold")}
             </ul>
           </div>
         }
@@ -79,7 +79,7 @@ const Navbar: React.FC<any> = ({
               <Image width={40} height={40} src="/icons/social-media.svg" />
             </label>
 
-            <ul className="menu menu-compact dropdown-content rounded-box shadow bg-black mt-9">
+            <ul className="menu menu-compact dropdown-content rounded-box shadow bg-black mt-9 p-3">
               <li><a rel="noopener noreferrer" href="https://github.com/Alvarian/" className={styles.tailwind.content.buttons.links} target="_blank"><Image width={45} height={45} src="/icons/github.svg" /></a></li>
               
               <li><a rel="noopener noreferrer" href="https://www.linkedin.com/in/alvarezivan88/" className={styles.tailwind.content.buttons.links} target="_blank"><Image width={30} height={30} src="/icons/linkedin.svg" /></a></li>
