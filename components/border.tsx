f61328160cc13e9f0ced6c4343419e87c410814b
Alvarian <vitarka88@gmail.com>
2022-05-28 11:07:50 -0400

    have footer be identified as Outro by border for slots, stage for animation

--- components/border.tsx
+++ components/border.tsx
@@ -1,12 +1,30 @@
 const Border: React.FC<any> = ({
     thickness,
-    color
+    color,
+    slotFields
 }) => {
+    const isVisible: boolean = slotFields.first.admissions?.isPermitted
+
     const styles = {
         tailwind: `${thickness} ${color} w-full z-20`
     }
+
+    const slot = () => {
+        if (isVisible) {
+            return (
+                <div>{slotFields.first.name}</div>
+            )
+        } 
+        
+        return (
+            <div>{slotFields.last.name}</div>
+        )
+    }
+    
     return (
-        <div className={styles.tailwind}></div>
+        <div className={styles.tailwind}>
+            {slot()}
+        </div>
     )
 }
 
