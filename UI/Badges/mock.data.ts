70f5fd414de7a68c84406f4e689bddc3bfbf91be
Alvarian <alvarivan88@gmail.com>
2022-06-06 06:31:27 -0400

    include mock data for badges, stage to render data into gui badges using daisyui

---
+++ UI/Badges/mock.data.ts
@@ -0,0 +1,8 @@
+export const badges = [
+  {
+    "name": "Javascript - Advanced",
+    "issuedOn": "2022-06-05T04:06:47Z",
+    "image": "https://images.credly.com/images/4dd00b35-94ff-48f3-9c75-7f438c72a3f9/6b8ce00dd356436eaac22f9efcfde552-01.png",
+    "evidence": "https://www.credly.com/badges/f6ad12ff-8713-42e9-ac6d-1ecaee245b6f",
+  }
+]
