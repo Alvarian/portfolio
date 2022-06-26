5826dc6dd53b7360c2980b1fd56a7bbe0f132873
Alvarian <vitarka88@gmail.com>
2022-06-26 09:39:02 -0400

    create badge revealing coat gif and align with badge crawl, stage to grid badges

--- UI/Badges/index.tsx
+++ UI/Badges/index.tsx
@@ -4,31 +4,69 @@ import { cloudBadges, localBadges } from "./mock.data"
 import styles from "./styles.module.css"
 
 
-const index: React.FC<any> = () => {
+const index: React.FC<any> = ({
+  isSectionPermitted
+}) => {
   const [isBadgeHovered, toggleBadgeSize] = useState(false)
+  const [coatPhase, setCoatPhase] = useState("start")
+  const badgeRevealTime = 1.35
+
+
+  useEffect(() => {
+
+    if (coatPhase === "start") {
+      setCoatPhase("run")
+    }
+  
+    if (isSectionPermitted) {
+      console.log("??", coatPhase)
+  
+  
+  
+      setTimeout(() => {setCoatPhase("end")}, badgeRevealTime*1000)
+    } else {
+      setCoatPhase("start")
+  
+    }
+
+  })
 
   const handleHoverBadge = () => {
     toggleBadgeSize(!isBadgeHovered)
   }
-
+  
   const {hexagon, image} = {
     hexagon: isBadgeHovered ? "300px" : "220px",
     image: isBadgeHovered ? "200px" : "150px"
   }
 
+  const renderBadgeCoatImg = () => {
+    console.log(coatPhase, isSectionPermitted)
+    switch (coatPhase) {
+      case "start":
+        return (<img src="./images/coat-start.gif" alt="coat" id={styles['badgeCoat']} />)
+      case "run":
+        return (<img src="./images/coat-run.gif" alt="coat" id={styles['badgeCoat']} />)
+      case "end":
+        return (<img src="./images/coat-end.gif" alt="coat" id={styles['badgeCoat']} />)
+    }
+  }
+
   return (
     <div className="h-full w-full flex items-center justify-end">
+      {renderBadgeCoatImg()}
+
       <div id={styles['crawl-container']}>
-        <motion.div id={styles['crawl']}
+        {isSectionPermitted && <motion.div id={styles['crawl']}
           initial={{
             x: "-100%",
             rotateY: 106,
           }}
-          whileInView={{
+          animate={{
             rotateY: 0,
             transition: {
-              delay: 1,
-              duration: 2,
+              delay: 0.162,
+              duration: badgeRevealTime,
               // type: "spring",
               // stiffness: 800,
               // damping: 100,
@@ -39,12 +77,9 @@ const index: React.FC<any> = () => {
           
           <div>
             {cloudBadges.map((badge: any, index: number) => (
-              <div key={index} className="relative flex justify-center items-center" 
+              <div key={index} className="relative flex justify-end items-center" 
                 style={{height: `${hexagon}`, width: `${hexagon}`}}
-                
               >
-                {/* <img src="./icons/badge.png" height="100%" width="100%" className="absolute -z-10 top-0 left-0" /> */}
-
                 <motion.a 
                   className="shadow shadow-lg border-8 rounded-full bg-black border-indigo-600 p-2"
                   id={styles['icons']} 
@@ -66,7 +101,7 @@ const index: React.FC<any> = () => {
               </div>
             ))}
           </div>
-        </motion.div>
+        </motion.div>}
       </div>                
     </div>  
   )
