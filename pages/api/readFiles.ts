2765ab6187ebc21e60f05096dbe8864d5a3652e3
Alvarian <vitarka88@gmail.com>
2022-06-26 13:19:28 -0400

    add gif frames to public, stage to replace coat openning methods with interval mapped solution

---
+++ pages/api/readFiles.ts
@@ -0,0 +1,24 @@
+import fs from 'fs'
+import path from 'path'
+import getConfig from 'next/config'
+
+export default (req: any, res: any) => {
+  const { serverRuntimeConfig } = getConfig()
+
+  const dirRelativeToPublicFolder = 'img'
+
+  const dir = path.join(serverRuntimeConfig.PROJECT_ROOT, './public', dirRelativeToPublicFolder);
+
+  const filenames = fs.readdirSync(dir);
+
+  const images = filenames.map(name => path.join('/', dirRelativeToPublicFolder, name))
+
+  res.statusCode = 200
+  res.json(images);
+}
+// next.config.js
+module.exports = {
+    serverRuntimeConfig: {
+        PROJECT_ROOT: __dirname
+    }
+}
