73d44e2725b3f208ad11edf237f5efb61225d305
Alvarian <vitarka88@gmail.com>
2022-07-26 21:31:07 -0400

    remove additional feature, stage to organize architecture before starting new section

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ module.exports = {
     domains: ['images.credly.com'],
     formats: ['image/avif', 'image/webp'],
   },
-  webpack5: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false, process: false, buffer: false, util: false, events: false, stream: false, crypto: false, dns: false, net: false, tls: false, querystring: false, assert: false, string_decoder: false};
 
