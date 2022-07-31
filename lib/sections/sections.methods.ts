17d0074a05035ed0b7032b5d5e7dee8e0d33ec9d
Alvarian <alvarivan88@gmail.com>
2022-07-31 00:41:15 -0400

    stylize and structure mock data into main project display, stage to eval function strings from mock

--- lib/sections/sections.methods.ts
+++ lib/sections/sections.methods.ts
@@ -36,4 +36,15 @@ export const rateLimiters = {
   debounce: (rate: number, callback: any) => _.debounce(callback, rate)
 }
 
+export const getFormattedDate = (blob: string) => {
+  const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+  const d = new Date(blob)
+  let monthName = month[d.getMonth()]
+  let day = d.getDay()
+  let year = d.getFullYear()
+
+  return `${monthName} ${day}, ${year}`
+}
+
 export const getFilesFromDir = async (host: string | undefined) => await (await fetch(`http://${host}/api/readFiles`)).json()
--- sections/Projects.tsx
+++ sections/Projects.tsx
@@ -1,9 +1,11 @@
-import { AnimatePresence, AnimateSharedLayout, motion } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
+import { getFormattedDate } from "lib/sections/sections.methods"
 import { FC, useState } from "react"
 
 
 interface Project {
     id: number,
+    title: string,
     icon: string,
     description: string,
     stacks: Array<string>,
@@ -21,7 +23,7 @@ const ProductImage: FC<{
             src={project.icon}
             alt=""
             onClick={() => onExpand(project)}
-            className="m-[5px] w-[140px] rounded-xl border-8 cursor-pointer transition z-10 ease-in-out hover:z-10 hover:scale-105 border-indigo-600"
+            className="m-[5px] w-[140px] rounded-xl border-8 cursor-pointer border-indigo-600"
             layoutId={`product-${project.id}`}
         />
     )
@@ -46,17 +48,33 @@ const index: FC<{data: Array<Project>}> = ({ data }) => {
         <div className="m-auto flex flex-row items-center h-[620px]">
             <main className="h-[620px] min-w-[880px] relative mr-[40px]">
                 <AnimatePresence>
-                    <motion.img
-                        key={primaryProduct.id}
-                        className="object-cover h-full w-full absolute top-0 left-0"
-                        src={primaryProduct.icon}
-                        alt=""
+                    <motion.div
                         layoutId={`product-${primaryProduct.id}`}
-                    />
+                        key={primaryProduct.id}
+                        className="object-cover h-full w-full absolute top-0 left-0 flex justify-around items-center rounded-2xl bg-black/50 hover:bg-black"
+                    >
+                        <div>
+                            <h2 className="text-4xl">{primaryProduct.title.charAt(0).toUpperCase() + primaryProduct.title.slice(1)}</h2>
+
+                            <img
+                                className="h-[400px] m-5"
+                                src={primaryProduct.icon}
+                                alt=""
+                            />
+
+                            <button className="btn btn-accent btn-wide">OPEN PROJECT</button>
+                        </div>
+
+                        <ul className="w-1/3 h-3/4 text-left flex flex-col justify-around items-center">
+                            <li className="w-full">{primaryProduct.description}</li>
+                            <li className="w-full"><a className="link link-primary" href={primaryProduct.repo}>Repository Source</a></li>
+                            <li className="w-full">Last push on {getFormattedDate(primaryProduct.lastUpdate)}</li>
+                        </ul>
+                    </motion.div>
                 </AnimatePresence>
             </main>
 
-            <aside className="flex flex-col flex-wrap overflow-auto h-[620px] w-[220px] mt-0 border-transparent border-4">
+            <aside className="flex flex-col flex-wrap h-[620px] w-[220px] overflow-auto mt-0">
                 <AnimatePresence>
                     {productIds.map((project: Project) => (
                         <ProductImage project={project} key={project.id} onExpand={setAsPrimary} />
