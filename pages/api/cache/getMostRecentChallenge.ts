28f4ae87f92b2172730b98fc84d3b4ad3f26362a
Ivan Alvarez <vitarka88@gmail.com>
2022-08-01 21:04:02 -0400

    add api route to test run redis usage from worker

---
+++ pages/api/cache/getMostRecentChallenge.ts
@@ -0,0 +1,29 @@
+import Redis from 'ioredis'
+
+export default (req: any, res: any) => {
+    try {
+        const redis: any = await (new Promise((resolve, reject) => {
+            const client = new Redis(process.env.NEXT_PUBLIC_REDIS_URL)
+            client.on("error", function (err: any) {
+              reject({
+                line: 224,
+                file: "pages/index",
+                time: now.minimal,
+                msg: "redis connection is unaccepted"
+              })
+            })
+      
+            client.on("ready", function () {
+                resolve(client)
+            })
+        }))
+
+        const mostRecentChallenge = await redis.get('mostRecentSolution')
+  
+        res.statusCode = 200
+        res.json(mostRecentChallenge);
+    } catch (err) {
+        console.log(err)
+        res.statusCode = 400
+    }
+}
