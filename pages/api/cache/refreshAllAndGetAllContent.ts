a983b92cf56acf04a12186edb9b2e0319e80e4ba
Alvarian <vitarka88@gmail.com>
2022-08-13 13:36:34 -0400

    migrate fetching core logics to helpers, then have main point of call to endpoint from getServerSideProps

---
+++ pages/api/cache/refreshAllAndGetAllContent.ts
@@ -0,0 +1,87 @@
+import { getStats, getBadges, getProjects } from "helpers/apiFetches"
+import Redis from "ioredis"
+import { formatDate } from "lib/sections/sections.methods"
+import moment from "moment-timezone"
+
+
+export default async (req: any, res: any) => {
+    const notifications: {
+        error: unknown,
+        warnings: unknown[]
+    } = {
+        error: null,
+        warnings: []
+    }
+    
+    const now = formatDate(moment.tz(new Date(), "America/New_York").format())  
+
+    try {
+        if (process.env.NEXT_PUBLIC_REDIS_URL) throw {
+            line: 35,
+            file: "pages/api/cache/refreshAllAndGetAllContent",
+            msg: "Missing Redis Credentials"
+        }
+
+        const redis: any = await (new Promise((resolve, reject) => {
+            const client = new Redis(process.env.NEXT_PUBLIC_REDIS_URL as string)
+            client.on("error", function () {
+                reject({
+                    line: 22,
+                    file: "pages/api/cache/refreshAllAndGetAllContent",
+                    msg: "redis connection is unaccepted"
+                })
+            })
+        
+            client.on("ready", function () {
+                resolve(client)
+            })
+        }))
+
+        const hasAllCachedContent = await (async () => {
+            const cachedPayload = await redis.get('portfolioCache')
+            if (!cachedPayload) return null
+            
+            const mappedCache = new Map()
+            for (const [key, val] of Object.entries(JSON.parse(cachedPayload).data)) {
+                mappedCache.set(key, val)
+            }
+
+            return mappedCache
+        })()
+        
+        let isDifferent: boolean = false
+        const makeDifferenceTrue = () => {if (!isDifferent) {isDifferent=!isDifferent}}
+        const payload = {
+            setting: 'external',
+            data: {
+                stats: await getStats(makeDifferenceTrue, hasAllCachedContent?.get('stats'), redis, notifications, now),
+                knowledge: await getBadges(makeDifferenceTrue, hasAllCachedContent?.get('knowledge'), redis, now),
+                projects: await getProjects(makeDifferenceTrue, hasAllCachedContent?.get('projects'), now)
+            },
+        }
+        
+        isDifferent && await redis.set('portfolioCache', JSON.stringify(payload))
+
+        res.statusCode = 200
+        res.json(payload)
+    } catch (err) {
+        notifications.error = err
+        
+        res.statusCode = 400
+        res.json({
+          setting: "local",
+          err
+        })
+    } finally {
+        if ((notifications.error || notifications.warnings.length) && process.env.NEXT_PUBLIC_NOTIFICATION_MAILING_SERVICE) {
+          fetch(process.env.NEXT_PUBLIC_NOTIFICATION_MAILING_SERVICE, {
+            method: "POST",
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(notifications)
+          })
+            .catch((err: unknown) => console.log(err))
+        }
+    }
+}
