dfd7efb68f9f10d4d1262687d9ca9be7b84dff79
alvarian <alvarivan88@gmail.com>
2024-02-27 08:59:55 -0500

    modal had children but no typing - got away with crime back then. also linted anys

---
+++ shared/types.ts
@@ -0,0 +1,10 @@
+export interface Project {
+    id: number,
+    title: string,
+    icon: string,
+    description: string,
+    stacks: Array<string>,
+    repo: string,
+    lastUpdate: string,
+    payload: {type: string, ref: Array<any> | string}
+}
